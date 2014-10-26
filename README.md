2c121e3bc018a569627edfbe82f2ad6e0d78e631
teone <teo.punto@gmail.com>
2014-10-26 21:33:54 +0100

    Start

---
+++ README.md
@@ -0,0 +1,6 @@
+#intToHours
+
+This is simple AngularJS filter that is usefull to convert integer into Hours and Minutes.
+
+## Example
+This filer will conver 1.5 to 1:30
---
+++ intToHours.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('intToHours')
+.filter('intToHours', function () {
+    return function (input) {
+
+        if(input && typeof input !== 'undefined'){
+            var hours = parseInt(input, 10);
+            var minutes = (input - hours) * 60;
+
+            minutes = parseInt(minutes,10);
+
+            if(minutes.length === 1){
+                minutes = '0'+minutes;
+            }
+            
+            return hours + ':' + minutes;
+        }
+    };
+});
